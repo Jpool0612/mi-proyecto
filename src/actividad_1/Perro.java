8cfd83faf5872dacf1ac4ad72abc03ec02d6ba2c
MI PC <MI PC@DESKTOP-ES6HVOL>
2025-07-02 17:43:21 -0500

    Primer guardado

---
+++ src/actividad_1/Perro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad_1;
+
+/**
+ *
+ * @author MI PC
+ */
+public class Perro extends Mascota {
+    public Perro(String nombre) {
+        super(nombre);
+    }
+
+    public void ladrar() {
+        System.out.println(nombre + " dice: ¡Guau!");
+    }
+}
